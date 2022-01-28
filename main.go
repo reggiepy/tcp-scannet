d6df56e3c492b87dcf2a1dcddbaaa28f72ca0850
reggiepy <wangtongpy@foxmail.com>
2022-01-28 15:33:06 +0800

    (all): 并发支持

--- main.go
+++ main.go
@@ -3,17 +3,25 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
-	for i := 22; i < 120; i++ {
-		address := fmt.Sprintf("127.0.0.1:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			fmt.Printf("error creating connection: %d\n", i)
-			continue
-		}
-		_ = conn.Close()
-		fmt.Printf("Connect\n")
+	host := "127.0.0.1"
+	var wg sync.WaitGroup
+	for i := 22; i < 25535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			address := fmt.Sprintf("%s:%d", host, port)
+			conn, err := net.Dial("tcp", address)
+			wg.Done()
+			if err != nil {
+				//fmt.Printf("error creating connection: %d\n", port)
+				return
+			}
+			_ = conn.Close()
+			fmt.Printf("Connect: %d\n", port)
+		}(i)
 	}
+	wg.Wait()
 }
